feb44a01313efc8c46c213578d739d3f5d848c3d
Tanya <tanyaz.228.67@gmail.com>
2020-05-29 18:30:09 +0300

    final

--- frontend/src/app/modules/pages/users/component/users.component.ts
+++ frontend/src/app/modules/pages/users/component/users.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from "rxjs";
 import {User} from "../../../../models/user";
 import {UserService} from "../../../../services/user.service";
+import {SubscriptionsService} from "../../../../services/subscriptions.service";
 
 @Component({
   selector: 'app-users',
@@ -13,15 +14,26 @@ export class UsersComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription[] = [];
   public users: User[] = [];
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService,
+              private subsService: SubscriptionsService) {}
 
   ngOnInit(): void {
     this.getUsers();
   }
 
+  public getSubscriptions(): void {
+    this.users.forEach(user => {
+      this.subscriptions.push(this.subsService.getSubscribers(user.id).subscribe(subUsers => {
+        console.log(user);
+        console.log(subUsers.length);
+      }))
+    })
+  }
+
   private getUsers(): void {
     this.subscriptions.push(this.userService.getUsers().subscribe(users => {
       this.users = users;
+      this.getSubscriptions();
     }))
   }
 
