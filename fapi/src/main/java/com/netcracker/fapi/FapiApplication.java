75da8e1b6942f83431bbb6c60b0ccbde7bbd7291
Tanya <tanyaz.228.67@gmail.com>
2020-11-27 02:34:17 +0300

    feat: swagger with jwt autorization

--- fapi/src/main/java/com/netcracker/fapi/FapiApplication.java
+++ fapi/src/main/java/com/netcracker/fapi/FapiApplication.java
@@ -2,8 +2,10 @@ package com.netcracker.fapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FapiApplication {
     public static void main(String[] args) {
         SpringApplication.run(FapiApplication.class, args);
