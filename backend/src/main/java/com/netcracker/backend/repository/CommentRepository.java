bdf2e530f503d4e1c9f19aa9d15c5a21b38de1e8
Tanya <tanyaz.228.67@gmail.com>
2020-11-27 02:58:35 +0300

    feat: add hql query

--- backend/src/main/java/com/netcracker/backend/repository/CommentRepository.java
+++ backend/src/main/java/com/netcracker/backend/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.netcracker.backend.repository;
 
 import com.netcracker.backend.entity.Comment;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +10,11 @@ import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, Long> {
+    @Query(value = "SELECT * FROM comment c where c.id = ?1", nativeQuery = true)
     Optional<Comment> findById(Long commentId);
+    @Query(value = "SELECT * FROM comment", nativeQuery = true)
     List<Comment> findAll();
+    @Query(value = "SELECT * FROM comment c where c.post_id = ?1", nativeQuery = true)
     List<Comment> findAllByPostId(Long postId);
     Comment save(Comment comment);
     void deleteById(Long commentId);
